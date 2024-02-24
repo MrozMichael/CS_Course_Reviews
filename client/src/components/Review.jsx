a83199a4101a1ab1ca22b055660e96e47144e32c
Michael Mroz <michaelmroz01111@gmail.com>
2024-02-24 14:47:45 -0400

    create basic Review component, todo: create schema for it in backend

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -3,5 +3,15 @@
 //assume each review object has content, rating, difficulty, instructor
 //and date
 const Review = ( {review} ) => {
-    
+    return(
+        <div className="review">
+            <p>Term Taken: {review.date}</p>
+            <p>Instructor Name: {review.instructor}</p>
+            <p>Course Rating: {review.rating} / 5 </p>
+            <p>Course Difficulty: {review.difficulty} / 5</p>
+            <p>{review.content}</p>
+        </div>
+    )
 }
+
+export default Review;
